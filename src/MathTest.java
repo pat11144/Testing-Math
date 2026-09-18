3e079686e3d7ad68efaa6d0f1aed6dd98a4a8015
pat11144 <128dpatel@frhsd.com>
2026-09-18 12:32:13 -0400

    test Dividing

--- src/MathTest.java
+++ src/MathTest.java
@@ -64,10 +64,70 @@ public class MathTest {
             "-7.4, -8.3, -9.2"
     })
     public void testMultiplying(double base, double a, double b) {
-        Assertions.assertEquals(Math.pow(base,a) * Math.pow(base, b), Math.pow(base, (a+b)));
+        Assertions.assertEquals(Math.pow(base,a) * Math.pow(base, b), Math.pow(base, (a+b)), 1E-9);
 
         //xa * xb = xa+b
     }
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5, 17",
+            "5, 2, -3",
+            "3, -3, -4",
+            "-7, 2, 4",
+            "-4, -5, 2",
+            "-7, -8, -9",
+            "6.3, 5.5, 17.3",
+            "5.25, 2.4, -3.2",
+            "3.4, -3.3, -4.2",
+            "-7.5, 2.2, 4.6",
+            "-4.3, -5.2, 2.1",
+            "-7.4, -8.3, -9.2"
+    })
+    public void testDividing(double base, double a, double b) {
+        Assertions.assertEquals( (Math.pow(base, a)/Math.pow(base, b)),Math.pow(base, (a-b)), 1E-9);
+        //xa / xb = xa-b
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5, 17",
+            "5, 2, -3",
+            "3, -3, -4",
+            "-7, 2, 4",
+            "-4, -5, 2",
+            "-7, -8, -9",
+            "6.3, 5.5, 17.3",
+            "5.25, 2.4, -3.2",
+            "3.4, -3.3, -4.2",
+            "-7.5, 2.2, 4.6",
+            "-4.3, -5.2, 2.1",
+            "-7.4, -8.3, -9.2"
+    })
+    public void testNestedPowers(double base, double a, double b) {
+        (xa)b = xa*b
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
+    public void testZeroPower(double base) {
+        x0 = 1
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
+    public void testOnePower(double base) {
+        x1 = x
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
+    public void testZeroBase(double exponent) {
+        0x = 0
+    }
+
+
 
 
 
