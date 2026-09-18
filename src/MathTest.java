7d21b209b28297c2969c6c489368f4d679adffa2
pat11144 <128dpatel@frhsd.com>
2026-09-18 12:20:21 -0400

    test Reciprocal

--- src/MathTest.java
+++ src/MathTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -7,7 +8,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MathTest {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5",
+            "1.5, 3.7",
+            "-3.4, -5.2",
+            "-2, -4",
+            "5, -6",
+            "3.5, -4.3",
+            "-4, 6",
+            "-6.4, 5.98",
+            "0, 0",
+            "0, -1.4",
+            "0, -4",
+            "0, 1.4",
+            "0, 4",
+            "1, 0",
+            "1, -1.4",
+            "1, -4",
+            "1, 1.4",
+            "1, 4",
+            "-1, 0",
+            "-1, -1.4",
+            "-1, -4",
+            "-1, 1.4",
+            "-1, 4",
+            "0, 1",
+            "-1.4, 1",
+            "-4, 1",
+            "1.4, 1",
+            "4, 1"
+        })
+    public void testReciprocal(double base, double exponent) {
+
+
+        Assertions.assertEquals(Math.pow(base, -exponent), 1/Math.pow(base, exponent));
+        //x-a = 1/xa;
+
+
+        }
+
+
 }
