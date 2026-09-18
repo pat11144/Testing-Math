bc72c49c920d0e040c67731cd880266caae1c4eb
pat11144 <128dpatel@frhsd.com>
2026-09-18 12:01:03 -0400

    Create Project

---
+++ src/MathTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
