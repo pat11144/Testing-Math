ddc61e20c1b33ba096804c2cf57cede37209547b
pat11144 <128dpatel@frhsd.com>
2026-09-18 12:25:26 -0400

    test Reciprocal

--- src/MathTest.java
+++ src/MathTest.java
@@ -48,5 +48,26 @@ public class MathTest {
 
         }
 
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5, 17",
+            "5, 2, -3",
+            "3, -3, -4",
+            "-7, 2, 4",
+            "-4, -5, 2",
+            "-7, -8, -9",
+            "6.3, 5.5, 17.3",
+            "5.25, 2.4, -3.2",
+            "3.4, -3.3, -4.2",
+            "-7.5, 2.2, 4.6",
+            "-4.3, -5.2, 2.1",
+            "-7.4, -8.3, -9.2"
+    })
+    public void testMultiplying(double base, double a, double b) {
+        Assertions.assertEquals(Math.pow(base,a) * Math.pow(base, b), Math.pow(base, (a+b)));
+        //xa * xb = xa+b
+    }
+
+
 
 }
